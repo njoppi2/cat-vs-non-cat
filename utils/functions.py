fcebfab6b2d912bc9bac4da223d94edc86075d47
Nelson Joppi <njoppi2@gmail.com>
2023-08-09 17:19:27 -0300

    do data augmentation and refactor code

--- utils/functions.py
+++ utils/functions.py
@@ -1,20 +1,35 @@
 import h5py
 import numpy as np
-
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 def load_dataset():
-    # h5 files contain multidimensional arrays, to load them to python use h5py.File
     train_dataset = h5py.File('data/train_catvnoncat.h5', "r")
     test_dataset = h5py.File('data/test_catvnoncat.h5', "r")
 
-    train_set_x = np.array(train_dataset["train_set_x"][:]) # your train set features
-    train_set_y = np.array(train_dataset["train_set_y"][:]) # your train set labels
+    train_set_x = np.array(train_dataset["train_set_x"][:])
+    train_set_y = np.array(train_dataset["train_set_y"][:])
 
-    test_set_x = np.array(test_dataset["test_set_x"][:]) # your test set features
-    test_set_y = np.array(test_dataset["test_set_y"][:]) # your test set labels
+    test_set_x = np.array(test_dataset["test_set_x"][:])
+    test_set_y = np.array(test_dataset["test_set_y"][:])
 
+    # Perform data augmentation on the training set
+    datagen = ImageDataGenerator(
+        rotation_range=20,
+        width_shift_range=0.1,
+        height_shift_range=0.1,
+        horizontal_flip=True
+    )
+    datagen.fit(train_set_x)  # Compute statistics for data augmentation
+
+    # Apply data augmentation transformations to training set and labels
+    augmented_images = datagen.flow(train_set_x, batch_size=len(train_set_x), shuffle=False).next()
+
+    # Concatenate augmented images and original images
+    train_set_x_augmented = np.concatenate((train_set_x, augmented_images))
+    train_set_y_augmented = np.concatenate((train_set_y, train_set_y))
+
+    # Flatten the images
     train_set_x_flat = train_set_x.reshape((train_set_x.shape[0], -1))
     test_set_x_flat = test_set_x.reshape((test_set_x.shape[0], -1))
 
-    return train_set_x, train_set_x_flat, train_set_y, test_set_x, test_set_x_flat, test_set_y
-
+    return train_set_x_augmented, train_set_x_flat, train_set_y_augmented, test_set_x, test_set_x_flat, test_set_y
