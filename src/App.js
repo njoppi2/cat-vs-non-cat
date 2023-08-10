11a6523e5d04b58ee9d659ebba9a6012a7e49cc7
Nelson Joppi <njoppi2@gmail.com>
2023-08-10 01:21:25 -0300

    improve UX

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import cat from './images/cat.png'
+import loadingGif from './images/loading.gif'
+
 
 function App() {
     const [selectedImage, setSelectedImage] = useState(null);
     const [classificationResult, setClassificationResult] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        console.log("selectedImage: " + !!selectedImage)
+        console.log("classificationResult: " + !!classificationResult)
+    }, [classificationResult, selectedImage]);
 
     const handleImageChange = (event) => {
         const file = event.target.files[0];
@@ -19,6 +28,7 @@ function App() {
     const handleImageUpload = async () => {
         if (selectedImage) {
             try {
+                setLoading(true);
                 const formData = new FormData();
                 formData.append('image', dataURItoBlob(selectedImage)); // Convert data URI to Blob
 
@@ -30,6 +40,7 @@ function App() {
                 const data = await response.json();
                 console.log('Response Data:', data);
                 setClassificationResult(data.predicted_class);
+                setLoading(false);
             } catch (error) {
                 console.error('Error uploading image:', error);
             }
@@ -51,19 +62,23 @@ function App() {
 
     return (
         <div className="App">
-            <div className="App-header">Cat classifier</div>
+            <div className="App-header">Cat classifier<p className="small">(Simple)</p></div>
             <div className="upload-row">
-                <p>Upload an image:&nbsp;</p>
+                <p className="upload-image">Upload an image:&nbsp;</p>
                 <input type="file" accept="image/*" onChange={handleImageChange} />
-                <button onClick={handleImageUpload}>Classify Image</button>
             </div>
-            {selectedImage && <img src={selectedImage} alt="Selected" />}
-            {classificationResult && (
-                <div className="result">
-                    <p>Classification Result:</p>
-                    <p>{classificationResult}</p>
-                </div>
-            )}
+            <button className="upload-row" onClick={handleImageUpload}>Classify Image</button>
+            {selectedImage
+                ?
+                <img src={selectedImage} alt="Selected" />
+                :
+                <img src={cat} alt="cat" />}
+            <div className="upload-row">
+                <p>Classification Result:&nbsp;</p>
+                <p>
+                    {!selectedImage ? "Cat" : (classificationResult === null) ? (loading && <img className="gif" src={loadingGif} alt="loading" />) : (classificationResult ? "Cat" : "Not a cat")}
+                </p>
+            </div>
         </div>
     );
 }
